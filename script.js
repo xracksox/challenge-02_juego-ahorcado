1480fb7d49c4772fb6b2e738f3d500ae7e5917c8
OscarD <xracksox@hotmail.com>
2022-09-08 00:23:51 -0500

    Logica lista

--- script.js
+++ script.js
@@ -1,78 +1,302 @@
-//// Replanteando xd
+//// Re Re Re replanteando :(
 
-let selectorPalabraSecreta = document.querySelector("#iniciar")
-let presiondeteclas = document.body;
-
-var pantalla = document.querySelector("canvas");
-var pincel = pantalla.getContext("2d");
-
-
-let palabraSecreta = ["alura", "oracle", "ahorcado", "secreto"];
-let palabraJuego;
-let letrasJuego = [];
-
-///función que devuelve palabra aleatoria
-function seleccionarPalabraSecreta() {
-    let aleatorio = Math.floor(Math.random() * palabraSecreta.length);
-    let palabraJuego = palabraSecreta[aleatorio];
-    areaJuego();
-    return palabraJuego;
-}
-///crea el array de la palabra aleatoria
-function arrayPalabraSecreta() {
-    let palabraSeleccionada = seleccionarPalabraSecreta().toUpperCase();
-    let letrasJuego = palabraSeleccionada.split("")
-    game(letrasJuego);
-    return letrasJuego;
-}
-///Dibuja el tablero de juegp
-function areaJuego() {
-    pincel.font = "50px Montserrat";
-    pincel.fillStyle = "lightblue";
-    pincel.fillRect(0, 0, 1200, 800);
-    pincel.fillStyle = "black";
-    pincel.fillRect(0, 600, 1280, 100)
-}
-////Funcion dibujar líneas de palabra
-function dibujarLineaPalabraSecreta(x, y) {
-    pincel.strokeStyle = "red";
-    pincel.lineWidth = 5;
-    pincel.beginPath();
-    pincel.moveTo(x, y);
-    pincel.lineTo(x + 50, y);
-    pincel.stroke();
-}
-////Dibuja guiones dependiendo las letras
-function game(letrasJuego) {
-    let ycoord = 690;
-    let lineasPalabra = letrasJuego;
-    console.log(lineasPalabra)
-    for (let x = 1; x <= lineasPalabra.length; x++) {
-        let xcoord = x * 80;
-        dibujarLineaPalabraSecreta(xcoord, ycoord)
+    let botonInicio = document.querySelector("#iniciar");
+    let nuevoJuego = document.querySelector(".juego--nuevo");
+    let presiondeteclas = document.querySelector(".cuerpo");
+    let guardarPalabra = document.querySelector("#guardar_palabra");
+    let botonAgregar = document.querySelector("#agregarPalabra");
+    
+    ////Pantallas
+    let pantallaInicio = document.querySelector("#inicio");
+    let pantallaAgregarPalabra = document.querySelector("#agregar__palabra");
+    let pantallaJuego = document.querySelector("#juego");
+    
+    
+    //// Canvas
+    var pantalla = document.querySelector("#canvas");
+    var pincel = pantalla.getContext("2d");
+    
+    
+    
+    
+    
+    
+    let listadePalabras = ["Mexico", "Colombia", "Peru", "España", "Argentina"];
+    // let palabraIngresada;
+    let palabraSecreta;
+    let piezas = 0;
+    let arraydePalabraSecreta;
+    let letrasIncorrectas = [];
+    let aciertos = 0;
+    
+    
+    
+    
+    
+    ///Dibuja el tablero de juegp
+    function areaJuego() {
+        pincel.fillStyle = "lightblue";
+        pincel.fillRect(0, 0, 1200, 800);
+        // pincel.fillStyle = "black";
+        // pincel.fillRect(0, 600, 1280, 100);
     }
-}
-
-////Verificar si una tecla fue presionada
-function capturarTecla(tecla) {
-    let validacion = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnñopqrstuvwxyz";
-    let teclaPress = tecla.key;
-    let letraPress = teclaPress.toUpperCase();
-
-    console.log(letraPress);
-}
-
-
-
-
-
-
-
-
-
-
-
-selectorPalabraSecreta.addEventListener("click", arrayPalabraSecreta);
-presiondeteclas.addEventListener("keydown", capturarTecla);
-
-
+    ////Funcion dibujar líneas de palabra
+    function dibujarLineaPalabraSecreta(x, y) {
+        pincel.strokeStyle = "red";
+        pincel.lineWidth = 5;
+        pincel.beginPath();
+        pincel.moveTo(x, y);
+        pincel.lineTo(x + 50, y);
+        pincel.stroke();
+    }
+    ////Función para dibujar guines respectivos a la palabra
+    function guionesPalabraSecreta() {
+        let ycoord = 710;
+        for (let x = 0; x < arraydePalabraSecreta.length; x++) {
+            let xcoord = (x + 1) * 80;
+            console.log(xcoord);
+            dibujarLineaPalabraSecreta(xcoord, ycoord);
+        }
+    }
+    ////Función para dibujar letra Correcta
+    function dibujarLetraCorrecta(letra, xpos, ypos) {
+        pincel.fillStyle = "green";
+        pincel.font = "50px Montserrat";
+        pincel.fillText(letra, xpos, ypos)
+    }
+    ////dibujar letra Incorrecta
+    function dibujarLetraIncorrecta(letrab, xposb, yposb) {
+        pincel.fillStyle = "red";
+        pincel.font = "30px Montserrat";
+        pincel.fillText(letrab, xposb, yposb)
+    }
+    //// Función Dibujar Ahorcado;
+    function horca() {
+        pincel.strokeStyle = "red";
+        pincel.lineWidth = 6;
+        pincel.beginPath();
+        pincel.moveTo(250, 600);
+        pincel.lineTo(850, 600);
+        pincel.stroke();
+        pincel.moveTo(330, 600);
+        pincel.lineTo(330, 50);
+        pincel.lineTo(600, 50);
+        pincel.lineTo(600, 100);
+        pincel.stroke();
+    }
+    function cabeza() {
+        pincel.strokeStyle = "red";
+        pincel.lineWidth = 6;
+        pincel.beginPath();
+        pincel.arc(600, 155, 50, 0, 2 * 3.14);
+        pincel.stroke();
+    }
+    function tronco() {
+        pincel.strokeStyle = "red";
+        pincel.lineWidth = 6;
+        pincel.beginPath();
+        pincel.moveTo(600, 210);
+        pincel.lineTo(600, 400);
+        pincel.stroke();
+    }
+    function piernaIzquierda() {
+        pincel.strokeStyle = "red";
+        pincel.lineWidth = 6;
+        pincel.beginPath();
+        pincel.moveTo(600, 405);
+        pincel.lineTo(525, 505);
+        pincel.stroke();
+    }
+    function piernaDerecha() {
+        pincel.strokeStyle = "red";
+        pincel.lineWidth = 6;
+        pincel.beginPath();
+        pincel.moveTo(600, 405);
+        pincel.lineTo(675, 505);
+        pincel.stroke();
+    }
+    function brazoIzquierdo() {
+        pincel.strokeStyle = "red";
+        pincel.lineWidth = 6;
+        pincel.beginPath();
+        pincel.moveTo(600, 260);
+        pincel.lineTo(525, 360);
+        pincel.stroke();
+    }
+    function brazoDerecho() {
+        pincel.strokeStyle = "red";
+        pincel.lineWidth = 6;
+        pincel.beginPath();
+        pincel.moveTo(600, 260);
+        pincel.lineTo(675, 360);
+        pincel.stroke();
+        pincel.fillStyle = "darkred";
+        pincel.font = "50px Montserrat";
+        pincel.fillText("Fin del Juego!", 800, 350);
+    
+    }
+    ////Dibuja el ahorcado
+    
+    function ahorcado() {
+        if (piezas == 1) {
+            horca();
+        }
+        if (piezas == 2) {
+            cabeza();
+        }
+        if (piezas == 3) {
+            tronco();
+        }
+        if (piezas == 4) {
+            piernaIzquierda();
+        }
+        if (piezas == 5) {
+            piernaDerecha();
+        }
+        if (piezas == 6) {
+            brazoIzquierdo();
+        }
+        if (piezas == 7) {
+            perdiste();
+        }
+    }
+    
+    
+    //// Funcion resetea todo e inicia el Juego
+    function iniciarJuego() {
+        selectordePalabra();
+        areaJuego();
+        guionesPalabraSecreta();
+    
+        piezas = 0;
+        palabraSecreta;
+        arraydePalabraSecreta;
+        letrasIncorrectas = [];
+        aciertos = 0;
+    
+    
+    
+    
+    
+        presiondeteclas.addEventListener("keydown", capturarTecla);
+    
+    }
+    ////Función para seleccionar una palabra aleatoria del Array
+    function selectordePalabra() {
+        let aleatorio = Math.floor(Math.random() * listadePalabras.length)
+        palabraSecreta = listadePalabras[aleatorio].toUpperCase();
+        console.log(`palabraSecreta: ${palabraSecreta}`);
+        arraydePalabraSecreta = palabraSecreta.split("");
+        console.log(`Array: ${arraydePalabraSecreta}`);
+    }
+    ////Fucnion para capturar las presiones del teclado
+    function capturarTecla(letra) {
+        let validacion = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnñopqrstuvwxyz";
+        teclaPresionada = letra.key.toUpperCase();
+        if (validacion.includes(teclaPresionada)) {
+            esunaLetra();
+        } else {
+            console.log("No has presionado una letra");
+        }
+    }
+    ///Función para saber si la letra es parte de la palabra
+    function esunaLetra() {
+        if (arraydePalabraSecreta.includes(teclaPresionada)) {
+            console.log(`la letra: ${teclaPresionada} SI es parte de la palabra`);
+            letraCorrecta();
+        } else {
+            console.log(`la letra: ${teclaPresionada} NO es parte de la palabra`);
+            letrasIncorrectas.push(teclaPresionada);
+            console.log(letrasIncorrectas);
+            letraIncorrecta(teclaPresionada);
+        }
+    
+    }
+    /// Función para saber si la letra ingresada es parte de la palabra
+    function letraCorrecta() {
+        palabra = arraydePalabraSecreta; // array de la palabra secreta
+        let ypos = 705;
+        for (let p = 0; p < palabra.length; p++) {
+            if (palabra[p] == teclaPresionada) {
+                console.log(teclaPresionada);
+                console.log(p);
+                letra = teclaPresionada;
+                aciertos++;
+                if (teclaPresionada == "I") {
+                    xpos = (p + 1) * 84;
+                    dibujarLetraCorrecta(letra, xpos, ypos);
+    
+                } else {
+                    xpos = (p + 1) * 81;
+                    dibujarLetraCorrecta(letra, xpos, ypos);
+                }
+            }
+            if (aciertos === palabra.length) {
+                ganaste();
+            }
+    
+        }
+    
+    }
+    ////Función para dibujar letra Incorrecta
+    function letraIncorrecta(teclaPresionada) {
+        letrab = teclaPresionada;
+        let yposb = 780;
+        for (let b = 0; b < letrasIncorrectas.length; b++) {
+            if (letrasIncorrectas[b] == teclaPresionada) {
+                xposb = (b + 1) * 60;
+                dibujarLetraIncorrecta(letrab, xposb, yposb);
+                piezas++;
+                console.log(piezas);
+                ahorcado();
+            }
+        }
+    }
+    ////Funcion dibuja el texto de ganaste
+    function mensajeGanaste() {
+        pincel.fillStyle = "darkgreen";
+        pincel.font = "50px Montserrat";
+        pincel.fillText("Ganaste,", 750, 150);
+        pincel.fillText("felicidades!", 740, 220);
+    }
+    
+    ////Funcion cuando Ganas La partida
+    function ganaste() {
+        horca();
+        mensajeGanaste();
+        presiondeteclas.removeEventListener("keydown", capturarTecla);
+    
+    }
+    //// Función cuando pierdes la partida
+    function perdiste() {
+        brazoDerecho();
+        presiondeteclas.removeEventListener("keydown", capturarTecla);
+    }
+    ////Función para agregar nuevas palabras
+    function agregarPalabra() {
+        palabraIngresada = document.querySelector("#textNueva").value;
+        ingresadaNormal = palabraIngresada.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+        console.log(ingresadaNormal)
+        listadePalabras.push(ingresadaNormal);
+        console.log(listadePalabras);
+        iniciarJuego();
+    }
+    
+    function pantallaAgregar() {
+        
+    }
+    
+    
+    
+    ////////////////////////////////////////////////////////////////////////////////
+    
+    
+    ////////////////////////////////////////////////////////////////////////////////
+    
+    
+    
+    
+    botonInicio.addEventListener("click", iniciarJuego);
+    nuevoJuego.addEventListener("click", iniciarJuego);
+    guardarPalabra.addEventListener("click", agregarPalabra);
+    botonAgregar.addEventListener("click", pantallaAgregar)
